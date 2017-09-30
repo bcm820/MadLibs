51bb7dc60d63f1475e68642f2b8fce77d46a668b
bcmendoza <31445238+bcmendoza@users.noreply.github.com>
2017-09-30 17:06:00 -0400

    Updated JS file

--- madlibs-app.js
+++ madlibs-app.js
@@ -1,100 +1,105 @@
 function madLibs(){
-
-    // Create parts-of-speech container
-    var inputs = [];
-    inputs[0] = "Enter a noun (i.e. prankster)"
-    inputs[1] = "Enter a pronoun (i.e. he)"
-    inputs[2] = "Enter a verb (i.e. laugh)"
-    inputs[3] = "Enter an adjective (i.e. tricky)"
-    inputs[4] = "Enter an adverb (i.e. crazily, trickily)"
-    inputs[5] = "Enter a preposition (e.g. by, with, about, until)"
-    inputs[6] = "Enter a conjunction (Joins phrases, e.g. and, but, because)"
-    inputs[7] = "Enter an interjection (Expresses emotion, e.g. 'Wow!' or 'Doh!')"
     
-    // Create word log containers
-    var nouns = [];
-    var pronouns = [];
-    var verbs = [];
-    var adjectives = [];
-    var adverbs = [];
-    var prepositions = [];
-    var conjunctions = [];
-    var interjections = [];
-    
-    // Request username and log greeting
-    var name = prompt("Please enter your username!");
-    console.log(`Hello ${name}! Let's write a random story!`)
-    
-    // Entering the loop
-    for (var i = 1; i > 0; i++){
-        var trap = Math.floor(Math.random() * 100)
-        if (trap < 13){
-            var word = prompt(inputs[0]);
-            nouns.push(word);
-        }
-        else if (trap < 25){
-            var word = prompt(inputs[1]);
-            pronouns.push(word);
-        }
-        else if (trap < 38){
-            var word = prompt(inputs[2]);
-            verbs.push(word);
-        }
-        else if (trap < 51){
-            var word = prompt(inputs[3]);
-            adjectives.push(word);
-        }
-        else if (trap < 64){
-            var word = prompt(inputs[4]);
-            adverbs.push(word);
-        }
-        else if (trap < 76){
-            var word = prompt(inputs[5]);
-            prepositions.push(word);
-        }
-        else if (trap < 88){
-            var word = prompt(inputs[6]);
-            conjunctions.push(word);
-        }
-        else if (trap < 100){
-            var word = prompt(inputs[7]);
-            interjections.push(word);
-        }
-        // Break out of the loop
-        if (word = null){
-            break;
-        }
-    }
-
-    // Define ratings based on total word count
-    var rating = ""
-    if (i < 11){
-        rating = "They were not very patient."
-    }
-    else if (i < 20){
-        rating = "They were very patient."
-    }
-    else if (i >= 20){
-        rating = "They were very gullible."
-    }
-    
-    // Print results
-    console.log(`
-        STORY:
-        There once was a person named ${name}.
-        ${rating}
+        // Create parts-of-speech container
+        var inputs = [];
+        inputs[0] = "Enter a noun (i.e. prankster)"
+        inputs[1] = "Enter a pronoun (i.e. he)"
+        inputs[2] = "Enter a verb (i.e. laugh)"
+        inputs[3] = "Enter an adjective (i.e. tricky)"
+        inputs[4] = "Enter an adverb (i.e. crazily, trickily)"
+        inputs[5] = "Enter a preposition (e.g. by, with, about, until)"
+        inputs[6] = "Enter a conjunction (Joins phrases, e.g. and, but, because)"
+        inputs[7] = "Enter an interjection (Expresses emotion, e.g. 'Wow!' or 'Doh!')"
         
-        RESULT:
-        You entered a total of ${i} words.
-        Your nouns (${nouns.length}): ${nouns}
-        Your pronouns (${pronouns.length}): ${pronouns}
-        Your verbs (${verbs.length}): ${verbs}
-        Your adjectives (${adjectives.length}): ${adjectives}
-        Your adverbs (${adverbs.length}): ${adverbs}
-        Your prepositions (${prepositions.length}): ${prepositions}
-        Your conjunctions (${conjunctions.length}): ${conjunctions}
-        Your interjections (${interjections.length}): ${interjections}
-    `)    
-}
-
-madLibs();
+        // Create word log containers
+        var nouns = [];
+        var pronouns = [];
+        var verbs = [];
+        var adjectives = [];
+        var adverbs = [];
+        var prepositions = [];
+        var conjunctions = [];
+        var interjections = [];
+        
+        // Request username, log greeting, prep word container
+        var name = window.prompt("Please enter your username!");
+        alert(`Hello, ${name}! Let's write a 'random' story! Enter words into the prompts to create an epic adventure!`)
+        var word = "";
+        
+        // Entering the loop
+        for (var i = 1; i > 0; i++){
+            var trap = Math.floor(Math.random() * 100)
+            if (trap < 15){
+                word = window.prompt(inputs[0]);
+                nouns.push(word);
+            }
+            else if (trap < 25){
+                word = window.prompt(inputs[1]);
+                pronouns.push(word);
+            }
+            else if (trap < 40){
+                word = window.prompt(inputs[2]);
+                verbs.push(word);
+            }
+            else if (trap < 55){
+                word = window.prompt(inputs[3]);
+                adjectives.push(word);
+            }
+            else if (trap < 65){
+                word = window.prompt(inputs[4]);
+                adverbs.push(word);
+            }
+            else if (trap < 75){
+                word = window.prompt(inputs[5]);
+                prepositions.push(word);
+            }
+            else if (trap < 85){
+                word = window.prompt(inputs[6]);
+                conjunctions.push(word);
+            }
+            else if (trap < 100){
+                word = window.prompt(inputs[7]);
+                interjections.push(word);
+            }
+            if (i == 25){
+                alert(`${name}, hit 'Cancel' anytime to see how your story is shaping up!`)
+            }
+            if (word === null){
+                break;
+            }
+        }
+    
+        var rating = ""
+        if (i < 11){
+            rating = "They were not very patient! :/"
+        }
+        else if (i < 20){
+            rating = "They were very patient! ;)"
+        }
+        else if (i >= 20){
+            rating = "They were very gullible!!!"
+        }
+        
+        var results = (`
+            <h2>Haha!!! Here's your story...</h2>
+            <p>There once was a person named ${name}.<br>
+            ${rating}<br>&nbsp;</p>
+            <h2>Result:</h2>
+            <p>You entered a total of ${i} words.</p>
+            <ul>
+                <li>Nouns (${nouns.length}): ${nouns}</li>
+                <li>Pronouns (${pronouns.length}): ${pronouns}</li>
+                <li>Verbs (${verbs.length}): ${verbs}</li>
+                <li>Adjectives (${adjectives.length}): ${adjectives}</li>
+                <li>Adverbs (${adverbs.length}): ${adverbs}</li>
+                <li>Prepositions (${prepositions.length}): ${prepositions}</li>
+                <li>Conjunctions (${conjunctions.length}): ${conjunctions}</li>
+                <li>Interjections (${interjections.length}): ${interjections}</li>
+            </ul>
+        `)
+        
+        document.getElementById('story').innerHTML = results;
+        
+    }
+    
+    madLibs();
